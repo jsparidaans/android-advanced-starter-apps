775405e617ba402a386564273da824e60fbcac89
Joey Sparidaans <joey.sparidaans@myidbv.com>
2020-11-27 14:25:03 +0100

    added SongDetailFragment

--- SongDetail_start/app/src/main/java/com/example/android/songdetailstart/SongDetailActivity.java
+++ SongDetail_start/app/src/main/java/com/example/android/songdetailstart/SongDetailActivity.java
@@ -33,7 +33,6 @@ import com.example.android.songdetailstart.content.SongUtils;
 public class SongDetailActivity extends AppCompatActivity {
 
     // SongItem includes the song title and detail.
-    public SongUtils.Song mSong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,8 +49,6 @@ public class SongDetailActivity extends AppCompatActivity {
 
         // This activity displays the detail. In a real-world scenario,
         // get the data from a content repository.
-        mSong = SongUtils.SONG_ITEMS.get
-                (getIntent().getIntExtra(SongUtils.SONG_ID_KEY, 0));
         // Show the detail information in a TextView.
         if (mSong != null) {
             ((TextView) findViewById(R.id.song_detail))
---
+++ SongDetail_start/app/src/main/java/com/example/android/songdetailstart/SongDetailFragment.java
@@ -0,0 +1,72 @@
+package com.example.android.songdetailstart;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.android.songdetailstart.content.SongUtils;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SongDetailFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SongDetailFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    public SongUtils.Song mSong;
+
+    public SongDetailFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SongDetailFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SongDetailFragment newInstance(String param1, String param2) {
+        SongDetailFragment fragment = new SongDetailFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+        mSong = SongUtils.SONG_ITEMS.get(getActivity().getIntent().getIntExtra(SongUtils.SONG_ID_KEY, 0));
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_song_detail, container, false);
+        if (mSong != null) {
+            ((TextView) rootView.findViewById(R.id.song_detail)).setText(mSong.details);
+        }
+        return rootView;
+    }
+}
