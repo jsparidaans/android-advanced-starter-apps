aceecfa6bee501a3ab36792efa5e90bc7ee23ad3
Joey Sparidaans <joey.sparidaans@myidbv.com>
2020-11-30 14:16:19 +0100

    added list of sensor displaying

--- SensorSurvey/app/src/main/java/com/dhl_myid/sensorsurvey/MainActivity.java
+++ SensorSurvey/app/src/main/java/com/dhl_myid/sensorsurvey/MainActivity.java
@@ -1,14 +1,39 @@
 package com.dhl_myid.sensorsurvey;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Retrieve sensor manager from system service
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        //Retrieve list of all sensors
+        List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        //Get sensor names from list
+        StringBuilder sensorText = new StringBuilder();
+        for (Sensor currentSensor : sensorList) {
+            sensorText.append(currentSensor.getName()).append(System.getProperty("line.separator"));
+        }
+
+        //Set sensor names in text view
+        TextView sensorTextView = findViewById(R.id.sensor_list);
+        sensorTextView.setText(sensorText);
+
     }
 }
