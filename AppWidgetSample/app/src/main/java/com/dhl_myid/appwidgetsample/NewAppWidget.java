84486c2ce9c6e881323b613dc36e2b76fe201444
Joey Sparidaans <joey.sparidaans@myidbv.com>
2020-11-30 11:53:21 +0100

    added logic for handling button click and updating widget

--- AppWidgetSample/app/src/main/java/com/dhl_myid/appwidgetsample/NewAppWidget.java
+++ AppWidgetSample/app/src/main/java/com/dhl_myid/appwidgetsample/NewAppWidget.java
@@ -1,8 +1,10 @@
 package com.dhl_myid.appwidgetsample;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.widget.RemoteViews;
 
@@ -18,7 +20,7 @@ public class NewAppWidget extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-
+        //Get the shared preference file
         SharedPreferences sharedPreferences = context.getSharedPreferences(mSharedPrefFile, 0);
         int count = sharedPreferences.getInt(COUNT_KEY + appWidgetId, 0);
         count++;
@@ -30,9 +32,22 @@ public class NewAppWidget extends AppWidgetProvider {
         views.setTextViewText(R.id.appwidget_id, String.valueOf(appWidgetId));
         views.setTextViewText(R.id.appwidget_update, context.getResources().getString(R.string.date_count_format, count, dateString));
 
+        // Write to the shared preference file
         SharedPreferences.Editor prefEditor = sharedPreferences.edit();
         prefEditor.putInt(COUNT_KEY + appWidgetId, count);
         prefEditor.apply();
+
+        //Create int array with only the current widget ID
+        int[] ids = new int[]{appWidgetId};
+
+        //Create the action intent and set the widget id array as extra
+        Intent intent = new Intent(context, NewAppWidget.class);
+        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
+
+        // Create a pending intent to receive the broadcast and set the on click of the button
+        PendingIntent pendingUpdate = PendingIntent.getBroadcast(context, appWidgetId, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setOnClickPendingIntent(R.id.button_update, pendingUpdate);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
