542e0074c1d61b389a12f6d0ecd0da1d3d31b22e
Joey Sparidaans <joey.sparidaans@myidbv.com>
2020-12-01 16:47:57 +0100

    added textwatchers

--- CustomEditText/app/src/main/java/com/dhl_myid/customedittext/view/EditTextWithClear.java
+++ CustomEditText/app/src/main/java/com/dhl_myid/customedittext/view/EditTextWithClear.java
@@ -2,6 +2,8 @@ package com.dhl_myid.customedittext.view;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 
 import androidx.annotation.NonNull;
@@ -36,7 +38,31 @@ public class EditTextWithClear extends AppCompatEditText {
         clearButtonImage = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_clear_opaque_24, null);
         //TODO: clear text on click
         //TODO: show/hide clear button on text change
+        addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                showClearButton();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
 
+    }
+
+    private void showClearButton() {
+        setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, clearButtonImage, null);
+    }
+
+    private void hideClearButton() {
+        setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, null, null);
     }
 }
