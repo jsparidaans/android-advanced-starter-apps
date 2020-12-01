8ea777909d7b34ff71e5d44987b8183f2dff4a99
Joey Sparidaans <joey.sparidaans@myidbv.com>
2020-12-01 12:34:15 +0100

    retrieved location and set it to textview

--- WalkMyAndroid-Starter/app/src/main/java/com/example/android/walkmyandroid/MainActivity.java
+++ WalkMyAndroid-Starter/app/src/main/java/com/example/android/walkmyandroid/MainActivity.java
@@ -15,14 +15,75 @@
  */
 package com.example.android.walkmyandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    private static final String TAG = "MainActivity";
+
+    private Location lastLocation;
+    private TextView locationTextView;
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get location client
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // Get UI variables
+        Button locationButton = findViewById(R.id.button_location);
+        locationTextView = findViewById(R.id.textview_location);
+
+        // Set listeners
+        locationButton.setOnClickListener(v -> getLocation());
+    }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+        } else {
+            fusedLocationClient.getLastLocation().addOnSuccessListener(location -> {
+                if (location != null) {
+                    lastLocation = location;
+                    locationTextView.setText(
+                            getString(R.string.location_text,
+                                    lastLocation.getLatitude(),
+                                    lastLocation.getLongitude(),
+                                    lastLocation.getTime()));
+                } else {
+                    locationTextView.setText(R.string.no_location);
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLocation();
+                } else {
+                    Toast.makeText(this, R.string.location_permission_denied, Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
