5fc217d1964382a0bbe9b200f42be9a167b7f53a
Joey Sparidaans <joey.sparidaans@myidbv.com>
2020-12-02 16:54:19 +0100

    initial commit

--- ClippingExample/app/src/main/java/com/dhl_myid/clippingexample/MainActivity.java
+++ ClippingExample/app/src/main/java/com/dhl_myid/clippingexample/MainActivity.java
@@ -1,10 +1,13 @@
 package com.dhl_myid.clippingexample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    //FIXME: Verdergaan bij 11.1 Part C
+    // https://developer.android.com/codelabs/advanced-android-training-apply-clipping-to-canvas?index=..%2F..advanced-android-training&authuser=1#2
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
