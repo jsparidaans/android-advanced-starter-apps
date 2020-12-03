22117decea7a13b1872bc32b9fd25954347c474b
Joey Sparidaans <joey.sparidaans@myidbv.com>
2020-12-03 11:50:36 +0100

    set up canvas drawing and win condition

--- SurfaceViewExample/app/src/main/java/com/dhl_myid/surfaceviewexample/view/GameView.java
+++ SurfaceViewExample/app/src/main/java/com/dhl_myid/surfaceviewexample/view/GameView.java
@@ -3,6 +3,7 @@ package com.dhl_myid.surfaceviewexample.view;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
@@ -81,7 +82,40 @@ public class GameView extends SurfaceView implements Runnable {
 
     @Override
     public void run() {
-        //stub
+        Canvas canvas;
+        while (isRunning) {
+            if (surfaceHolder.getSurface().isValid()) {
+                //Get flashlight coordinates and radius
+                int x = flashLightCone.getX();
+                int y = flashLightCone.getY();
+                int radius = flashLightCone.getRadius();
+
+                //Set up canvas
+                canvas = surfaceHolder.lockCanvas();
+                canvas.save();
+
+                //Draw
+                canvas.drawColor(Color.WHITE);
+                canvas.drawBitmap(bitmap, bitmapX, bitmapY, paint);
+
+                //Add the flashlight
+                path.addCircle(x, y, radius, Path.Direction.CCW);
+                canvas.clipOutPath(path);
+
+                //Set background to black
+                canvas.drawColor(Color.BLACK);
+
+                //Win condition
+                if (x > winnerRect.left && x < winnerRect.right && y > winnerRect.top && y < winnerRect.bottom) {
+                    canvas.drawColor(Color.WHITE);
+                    canvas.drawBitmap(bitmap, bitmapX, bitmapY, paint);
+                    canvas.drawText("WIN!", (float) viewWidth / 3, (float) viewHeight / 2, paint);
+                }
+                path.rewind();
+                canvas.restore();
+                surfaceHolder.unlockCanvasAndPost(canvas);
+            }
+        }
     }
 
     @Override
