c972e4b85cca76994a020ed7097a3ff3d937bcb7
Joey Sparidaans <joey.sparidaans@myidbv.com>
2020-12-03 15:42:51 +0100

    implemented video view in MainActivity

--- SimpleVideoView/app/src/main/java/com/dhl_myid/simplevideoview/MainActivity.java
+++ SimpleVideoView/app/src/main/java/com/dhl_myid/simplevideoview/MainActivity.java
@@ -1,14 +1,37 @@
 package com.dhl_myid.simplevideoview;
 
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.VideoView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    //Constants
+    private static final String VIDEO_SAMPLE = "tacoma_narrows";
+
+    //Member variables
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        videoView=findViewById(R.id.video_view);
+    }
+
+    private void initializePlayer(){
+        Uri videoUri = getMedia(VIDEO_SAMPLE);
+        videoView.setVideoURI(videoUri);
+        videoView.start();
+    }
+
+    private void releasePlayer(){
+        videoView.stopPlayback();
+    }
+
+    private Uri getMedia(String mediaName) {
+        return Uri.parse("android.resource://" + getPackageName() + "/raw/" + mediaName);
     }
 }
