e58f673fc2f83b06b565fb85cb3d67c2041107b7
Joey Sparidaans <joey.sparidaans@myidbv.com>
2020-12-03 11:23:21 +0100

    set up the bitmap

--- SurfaceViewExample/app/src/main/java/com/dhl_myid/surfaceviewexample/view/GameView.java
+++ SurfaceViewExample/app/src/main/java/com/dhl_myid/surfaceviewexample/view/GameView.java
@@ -1,9 +1,11 @@
 package com.dhl_myid.surfaceviewexample.view;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -15,6 +17,12 @@ public class GameView extends SurfaceView implements Runnable {
     private SurfaceHolder surfaceHolder;
     private Paint paint;
     private Path path;
+    private Bitmap bitmap;
+    private int bitmapX, bitmapY;
+    private RectF winnerRect;
+    private double viewWidth;
+    private double viewHeight;
+
 
     public GameView(Context context) {
         super(context);
@@ -37,6 +45,14 @@ public class GameView extends SurfaceView implements Runnable {
         paint = new Paint();
         paint.setColor(Color.DKGRAY);
         path = new Path();
+    }
+
+    private void setUpBitmap() {
+        bitmapX = (int) Math.floor(Math.random() * (viewWidth - bitmap.getWidth()));
+        bitmapY = (int) Math.floor(Math.random() * (viewHeight - bitmap.getHeight()));
+        winnerRect = new RectF(bitmapX, bitmapY,
+                bitmapX + bitmap.getWidth(),
+                bitmapY + bitmap.getHeight());
     }
 
     public void pause() {
