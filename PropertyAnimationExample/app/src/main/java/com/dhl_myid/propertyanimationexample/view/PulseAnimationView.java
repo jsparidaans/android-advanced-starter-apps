4a4ae9af15c7823958346f0c780baea582875074
Joey Sparidaans <joey.sparidaans@myidbv.com>
2020-12-03 14:52:15 +0100

    added radius

--- PropertyAnimationExample/app/src/main/java/com/dhl_myid/propertyanimationexample/view/PulseAnimationView.java
+++ PropertyAnimationExample/app/src/main/java/com/dhl_myid/propertyanimationexample/view/PulseAnimationView.java
@@ -1,12 +1,25 @@
 package com.dhl_myid.propertyanimationexample.view;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class PulseAnimationView extends View {
+
+    //Constants
+    private static final int COLOR_ADJUSTER = 5;
+    private final Paint paint = new Paint();
+    private float radius;
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+        paint.setColor(Color.GREEN + (int) radius / COLOR_ADJUSTER);
+        invalidate();
+    }
 
     public PulseAnimationView(Context context) {
         this(context, null);
