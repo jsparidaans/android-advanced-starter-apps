0e7f85977d996b53cb4614d39750a44737f05033
Joey Sparidaans <joey.sparidaans@myidbv.com>
2020-12-03 15:09:36 +0100

    created animations

--- PropertyAnimationExample/app/src/main/java/com/dhl_myid/propertyanimationexample/view/PulseAnimationView.java
+++ PropertyAnimationExample/app/src/main/java/com/dhl_myid/propertyanimationexample/view/PulseAnimationView.java
@@ -1,19 +1,30 @@
 package com.dhl_myid.propertyanimationexample.view;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 
 import androidx.annotation.Nullable;
+import androidx.interpolator.view.animation.LinearOutSlowInInterpolator;
 
 public class PulseAnimationView extends View {
 
     //Constants
     private static final int COLOR_ADJUSTER = 5;
+    private static final int ANIMATION_DURATION = 4000;
+    private static final long ANIMATION_DELAY = 1000;
     private final Paint paint = new Paint();
-    private float radius;
+
+    private float radius, x, y;
+    private AnimatorSet pulseAnimatorSet = new AnimatorSet();
 
     public void setRadius(float radius) {
         this.radius = radius;
@@ -28,4 +39,39 @@ public class PulseAnimationView extends View {
     public PulseAnimationView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+            x = event.getX();
+            y = event.getY();
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onSizeChanged(int width, int height, int oldWidth, int oldHeight) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight);
+        ObjectAnimator growAnimator = ObjectAnimator.ofFloat(this, "radius", 0, getWidth());
+        growAnimator.setDuration(ANIMATION_DURATION);
+        growAnimator.setInterpolator(new LinearInterpolator());
+
+        ObjectAnimator shrinkAnimator = ObjectAnimator.ofFloat(this, "radius", getWidth(), 0);
+        shrinkAnimator.setDuration(ANIMATION_DURATION);
+        shrinkAnimator.setInterpolator(new LinearOutSlowInInterpolator());
+        shrinkAnimator.setStartDelay(ANIMATION_DELAY);
+
+        ObjectAnimator repeatAnimator = ObjectAnimator.ofFloat(this, "radius", 0, getWidth());
+        repeatAnimator.setStartDelay(ANIMATION_DELAY);
+        repeatAnimator.setDuration(ANIMATION_DURATION);
+        repeatAnimator.setRepeatCount(1);
+        repeatAnimator.setRepeatMode(ValueAnimator.REVERSE);
+
+        pulseAnimatorSet.play(growAnimator).before(shrinkAnimator);
+        pulseAnimatorSet.play(repeatAnimator).after(shrinkAnimator);
+    }
+
+
 }
