14da77df53a2430c815a0f380e1e586aeef01b48
Joey Sparidaans <joey.sparidaans@myidbv.com>
2020-12-03 12:31:58 +0100

    created PulseAnimationView

---
+++ PropertyAnimationExample/app/src/main/java/com/dhl_myid/propertyanimationexample/view/PulseAnimationView.java
@@ -0,0 +1,18 @@
+package com.dhl_myid.propertyanimationexample.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class PulseAnimationView extends View {
+
+    public PulseAnimationView(Context context) {
+        this(context, null);
+    }
+
+    public PulseAnimationView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
