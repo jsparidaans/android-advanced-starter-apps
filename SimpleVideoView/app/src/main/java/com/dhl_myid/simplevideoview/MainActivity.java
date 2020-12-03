e1d9792ad451770970d1713cf6be03f7d0c5e1b6
Joey Sparidaans <joey.sparidaans@myidbv.com>
2020-12-03 16:12:11 +0100

    added playback to saved instance state

--- SimpleVideoView/app/src/main/java/com/dhl_myid/simplevideoview/MainActivity.java
+++ SimpleVideoView/app/src/main/java/com/dhl_myid/simplevideoview/MainActivity.java
@@ -2,29 +2,61 @@ package com.dhl_myid.simplevideoview;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     //Constants
     private static final String VIDEO_SAMPLE = "tacoma_narrows";
+    private static final String PLAYBACK_TIME = "play_time";
 
     //Member variables
     private VideoView videoView;
+    private int currentPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         videoView = findViewById(R.id.video_view);
+
+        if (savedInstanceState != null) {
+            currentPosition = savedInstanceState.getInt(PLAYBACK_TIME);
+        }
+
+        MediaController controller = new MediaController(this);
+        controller.setMediaPlayer(videoView);
+        videoView.setMediaController(controller);
+
     }
 
     private void initializePlayer() {
         Uri videoUri = getMedia(VIDEO_SAMPLE);
         videoView.setVideoURI(videoUri);
+        // Restore view to previous play time
+        if (currentPosition > 0) {
+            videoView.seekTo(currentPosition);
+        } else {
+            //Setting to 1 shows the first frame in stead of a black square
+            videoView.seekTo(1);
+        }
         videoView.start();
+        videoView.setOnCompletionListener(mediaPlayer -> {
+            Toast.makeText(MainActivity.this, "Playback completed", Toast.LENGTH_SHORT).show();
+            videoView.seekTo(1);
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(PLAYBACK_TIME, videoView.getCurrentPosition());
     }
 
     private void releasePlayer() {
