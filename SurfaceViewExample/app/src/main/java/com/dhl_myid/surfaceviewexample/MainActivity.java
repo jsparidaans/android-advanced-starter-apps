a40d549eec055445365dc721b93fc64887500821
Joey Sparidaans <joey.sparidaans@myidbv.com>
2020-12-03 11:12:50 +0100

    set MainActivity content to GameView

--- SurfaceViewExample/app/src/main/java/com/dhl_myid/surfaceviewexample/MainActivity.java
+++ SurfaceViewExample/app/src/main/java/com/dhl_myid/surfaceviewexample/MainActivity.java
@@ -1,14 +1,35 @@
 package com.dhl_myid.surfaceviewexample;
 
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.dhl_myid.surfaceviewexample.view.GameView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GameView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        gameView = new GameView(this);
+        gameView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        setContentView(gameView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameView.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.resume();
     }
 }
