8e0dcb0d4d5878e8360c58d254c724b9e87ee8e7
Joey Sparidaans <joey.sparidaans@myidbv.com>
2020-12-03 16:23:03 +0100

    added URL video playing

--- SimpleVideoView/app/src/main/java/com/dhl_myid/simplevideoview/MainActivity.java
+++ SimpleVideoView/app/src/main/java/com/dhl_myid/simplevideoview/MainActivity.java
@@ -2,7 +2,10 @@ package com.dhl_myid.simplevideoview;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.URLUtil;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
@@ -12,11 +15,12 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     //Constants
-    private static final String VIDEO_SAMPLE = "tacoma_narrows";
+    private static final String VIDEO_SAMPLE = "https://developers.google.com/training/images/tacoma_narrows.mp4";
     private static final String PLAYBACK_TIME = "play_time";
 
     //Member variables
     private VideoView videoView;
+    private TextView bufferingView;
     private int currentPosition = 0;
 
     @Override
@@ -24,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         videoView = findViewById(R.id.video_view);
+        bufferingView = findViewById(R.id.buffering_text_view);
 
         if (savedInstanceState != null) {
             currentPosition = savedInstanceState.getInt(PLAYBACK_TIME);
@@ -36,16 +41,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initializePlayer() {
+        bufferingView.setVisibility(View.VISIBLE);
         Uri videoUri = getMedia(VIDEO_SAMPLE);
         videoView.setVideoURI(videoUri);
-        // Restore view to previous play time
-        if (currentPosition > 0) {
-            videoView.seekTo(currentPosition);
-        } else {
-            //Setting to 1 shows the first frame in stead of a black square
-            videoView.seekTo(1);
-        }
-        videoView.start();
+        videoView.setOnPreparedListener(mediaPlayer -> {
+            bufferingView.setVisibility(View.GONE);
+
+            // Restore view to previous play time
+            if (currentPosition > 0) {
+                videoView.seekTo(currentPosition);
+            } else {
+                //Setting to 1 shows the first frame in stead of a black square
+                videoView.seekTo(1);
+            }
+
+            videoView.start();
+        });
+
         videoView.setOnCompletionListener(mediaPlayer -> {
             Toast.makeText(MainActivity.this, "Playback completed", Toast.LENGTH_SHORT).show();
             videoView.seekTo(1);
@@ -85,6 +97,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private Uri getMedia(String mediaName) {
-        return Uri.parse("android.resource://" + getPackageName() + "/raw/" + mediaName);
+        if (URLUtil.isValidUrl(mediaName)) {
+            return Uri.parse(mediaName);
+        } else {
+            return Uri.parse("android.resource://" + getPackageName() + "/raw/" + mediaName);
+        }
     }
 }
