9a746ccccab2106bcf04dcc67ba91740ca0abbe7
Joey Sparidaans <joey.sparidaans@myidbv.com>
2020-12-03 11:02:49 +0100

    created a flashlight object

---
+++ SurfaceViewExample/app/src/main/java/com/dhl_myid/surfaceviewexample/view/FlashLightCone.java
@@ -0,0 +1,28 @@
+package com.dhl_myid.surfaceviewexample.view;
+
+public class FlashLightCone {
+    private int x, y, radius;
+
+    public FlashLightCone(int viewWidth, int viewHeight) {
+        x = viewWidth / 2;
+        y = viewHeight / 2;
+        radius = ((viewWidth <= viewHeight) ? x / 3 : y / 3);
+    }
+
+    public void update(int newX, int newY){
+        x=newX;
+        y=newY;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+}
